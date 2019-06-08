d9c9248bdd973c7449a972fc3df8b72863aca194
Nia Bert <nbert14@gmail.com>
2019-06-08 18:39:01 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,16 +1,28 @@
 class CashRegister
+<<<<<<< HEAD
   attr_accessor :total, :discount, :price
+=======
+  attr_accessor :total, :discount
+>>>>>>> 126fe36096c4b6864ad9e3bb4d757723aced94b2
   
   
   def initialize(discount = 0)
     @total = 0
     @discount = discount
+<<<<<<< HEAD
     @grocery_list = []
   end
   
   def add_item(item, price, quantity = 1)
     @price = price
     @grocery_list.concat([item]*quantity)
+=======
+    @items = []
+  end
+  
+  def add_item(item, price, quantity = 1)
+    @items << item
+>>>>>>> 126fe36096c4b6864ad9e3bb4d757723aced94b2
     @total += (price*quantity)
   end
   
@@ -23,10 +35,16 @@ class CashRegister
   end
   
   def items 
+<<<<<<< HEAD
     @grocery_list
   end
   
   def void_last_transaction
     @total -= @price
   end
+=======
+    @items
+  end
+  
+>>>>>>> 126fe36096c4b6864ad9e3bb4d757723aced94b2
 end
